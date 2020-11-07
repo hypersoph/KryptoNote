e0e282f35aed19618147405236486ee6837ff715
user <@>
2020-11-07 18:21:50 -0500

    .

--- app/src/main/java/ca/yorku/kryptonote/KryptoNoteActivity.java
+++ app/src/main/java/ca/yorku/kryptonote/KryptoNoteActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 
 public class KryptoNoteActivity extends AppCompatActivity
@@ -23,10 +24,37 @@ public class KryptoNoteActivity extends AppCompatActivity
     }
 
     public void onEncrypt(View v) {
+        Toast.makeText(this, "asdf",Toast.LENGTH_LONG).show();
+        try
+        {
+            String key = ((EditText) findViewById(R.id.key)).getText().toString();
+            Cipher cipher = new Cipher(key);
+            String note = ((EditText) findViewById(R.id.data)).getText().toString();
+            String encrypted = cipher.encrypt(note);
+
+            ((EditText) findViewById(R.id.data)).setText(encrypted);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
 
     }
 
     public void onDecrypt(View v) {
+        try {
+            String key = ((EditText) findViewById(R.id.key)).getText().toString();
+            Cipher cipher = new Cipher(key);
+            String note = ((EditText) findViewById(R.id.data)).getText().toString();
+            String decrypted = cipher.decrypt(note);
+
+            ((EditText) findViewById(R.id.data)).setText(decrypted);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
 
     }
 
@@ -39,15 +67,32 @@ public class KryptoNoteActivity extends AppCompatActivity
             FileWriter fw = new FileWriter(file);
             fw.write(((EditText) findViewById(R.id.data)).getText().toString());
             fw.close();
-            Toast.makeText(this,"Note Saved.",Toast.LENGTH_LONG);
+            Toast.makeText(this,"Note Saved.",Toast.LENGTH_LONG).show();
         }
         catch (Exception e)
         {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
 
     public void onLoad(View V) {
-        
+        try
+        {
+            String name = ((EditText) findViewById(R.id.file)).getText().toString();
+            File dir = this.getFilesDir();
+            File file = new File(dir, name);
+            FileReader fr = new FileReader(file);
+            String show ="";
+            for (int c = fr.read();c!=-1;c=fr.read()) {
+                show+= (char) c;
+            }
+            fr.close();
+            ((EditText) findViewById(R.id.data)).setText(show);
+            Toast.makeText(this,"Note Loaded.",Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
