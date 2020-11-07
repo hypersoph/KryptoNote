9a122235ec7eb4d847fa1b1027dcbe8f5d8373a7
user <@>
2020-11-07 17:51:42 -0500

    .

--- app/src/main/java/ca/yorku/kryptonote/KryptoNoteActivity.java
+++ app/src/main/java/ca/yorku/kryptonote/KryptoNoteActivity.java
@@ -5,6 +5,12 @@ package ca.yorku.kryptonote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
 
 public class KryptoNoteActivity extends AppCompatActivity
 {
@@ -14,5 +20,34 @@ public class KryptoNoteActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kryptonote_activity);
+    }
+
+    public void onEncrypt(View v) {
+
+    }
+
+    public void onDecrypt(View v) {
+
+    }
+
+    public void onSave(View v) {
+        try
+        {
+            String name = ((EditText) findViewById(R.id.file)).getText().toString();
+            File dir = this.getFilesDir();
+            File file = new File(dir, name);
+            FileWriter fw = new FileWriter(file);
+            fw.write(((EditText) findViewById(R.id.data)).getText().toString());
+            fw.close();
+            Toast.makeText(this,"Note Saved.",Toast.LENGTH_LONG);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG);
+        }
+    }
+
+    public void onLoad(View V) {
+        
     }
 }
